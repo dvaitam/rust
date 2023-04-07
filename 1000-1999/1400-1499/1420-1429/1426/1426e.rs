052ed0502f96d4ebcf9aa0279dac43146ca6d407
Anil M <amanchikatla@icloud.com>
2023-04-07 10:18:17 +0530

    3rd day of Rust.

--- 1000-1999/1400-1499/1420-1429/1426/1426e.rs
+++ 1000-1999/1400-1499/1420-1429/1426/1426e.rs
@@ -19,30 +19,20 @@ fn main() {
         .read_line(&mut line)
         .expect("Failed to read line");
     let mut split = line.trim().split(" ");
-    let (a1, a2, a3) = (
-        split.next().unwrap(),
-        split.next().unwrap(),
-        split.next().unwrap(),
-    );
     let (a1, a2, a3): (i32, i32, i32) = (
-        a1.parse().expect("not int"),
-        a2.parse().expect("not int"),
-        a3.parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
     );
     let mut line = String::new();
     io::stdin()
         .read_line(&mut line)
         .expect("Failed to read line");
     let mut split = line.trim().split(" ");
-    let (b1, b2, b3) = (
-        split.next().unwrap(),
-        split.next().unwrap(),
-        split.next().unwrap(),
-    );
     let (b1, b2, b3): (i32, i32, i32) = (
-        b1.parse().expect("not int"),
-        b2.parse().expect("not int"),
-        b3.parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
+        split.next().unwrap().parse().expect("not int"),
     );
 
     println!("{} {}",get_losses(a1, a2, a3, b1, b2, b3), get_wins(b1, b2, b3, a1, a2, a3));
