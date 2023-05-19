08b1ac3f71f27e6cdb2cf9a7dd7ac70977c6a793
Anil M <amanchikatla@icloud.com>
2023-05-19 09:57:27 +0530

    42nd day of Rust.

---
+++ 1000-1999/1100-1199/1180-1189/1186/1186c.rs
@@ -0,0 +1,58 @@
+use std::io;
+
+fn main() {
+   
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
+    let s : Vec<char> = line.trim().to_string().chars().collect();
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
+    let t : Vec<char> = line.trim().to_string().chars().collect();
+    let (mut m, mut n) = (true, true);
+	let (mut same, mut diff) = (true, true);
+	for i in 0..t.len() {
+		if s[i] == t[i] {
+			m = !m;
+		} else {
+			n = !n;
+		}
+		if i > 0 {
+			if t[i] == t[i-1] {
+				same = !same;
+			} else {
+				diff = !diff;
+			}
+		}
+	}
+	let mut ans = 0;
+	if n {
+		ans += 1;
+	}
+	//write(f, "ans ", ans, "\n")
+	for i in t.len()..s.len() {
+		if s[i-t.len()] == t[0] {
+			m = !m;
+		} else {
+			n = !n;
+		}
+		if !diff {
+			m = !m;
+			n = !n;
+		}
+		if s[i] == t[t.len()-1] {
+			m = !m;
+		} else {
+			n = !n;
+		}
+
+		if n {
+			ans+= 1;
+		}
+	}
+	println!("{}", ans);
+   
+}
